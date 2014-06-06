23b24f7576c726fb59523cbc7a807aed7be600b4
Brian Stevens <brian.stevens@dataporters.com>
2014-06-06 11:05:44 -0500

    Initial implementation of carbon-agent gem for Lightbridge

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -1,18 +1,6 @@
-require 'mysql'
 require 'net/http'
 
-class MetricNotAvailable < RuntimeError
-end
-
 module Metrics
-  def apache_active_workers
-    from_apache_mod_status("BusyWorkers").to_i
-  end
-
-  def apache_idle_workers
-    from_apache_mod_status("IdleWorkers").to_i
-  end
-  
   def filesystem_space
     `df -kP | grep "^/"`.split(/\n/).collect do |line|
       fields = line.split(/\s+/)
@@ -34,22 +22,6 @@ module Metrics
     from_proc_meminfo('MemTotal').gsub(" kB$", '').to_i
   end
   
-  def mysql_slave_lag
-    seconds = nil
-    begin
-      db = Mysql.new('localhost', 'carbon', '', '')
-      result = db.query('show slave status')
-      status = result.fetch_hash or raise MetricNotAvailable
-      seconds = status['Seconds_Behind_Master']
-      result.free
-    rescue Mysql::Error
-      raise MetricNotAvailable
-    ensure
-      db.close if db
-    end
-    seconds
-  end
-  
   def swap_free
     from_proc_meminfo('SwapFree').gsub(" kB$", '').to_i
   end
@@ -60,20 +32,6 @@ module Metrics
 
   private
   
-  def from_apache_mod_status(string)
-    begin
-      response = Net::HTTP.get_response('localhost', '/server-status?auto')
-      if response.code == '200' then
-        response.body.split(/\n/).detect {|line| line =~ /^#{Regexp.quote(string)}: (.*)/}
-        $1
-      else
-        raise MetricNotAvailable
-      end
-    rescue Errno::ECONNREFUSED
-      raise MetricNotAvailable
-    end
-  end
-
   def from_proc_meminfo(string)
     IO.read('/proc/meminfo').split(/\n/).detect {|line| line =~ /^#{Regexp.quote(string)}:\s+(.*)/}
     $1
